4cac604632464a4f145ece10067056e3acf54004
portseif <chris@portseif.io>
2024-07-13 22:42:23 -0400

    Chirp CRUD operational test

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany( Chirp::class );
+    }
+
     use HasFactory, Notifiable;
 
     /**
@@ -41,7 +48,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'          => 'hashed',
         ];
     }
 }
